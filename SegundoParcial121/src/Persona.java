423ff080186a8c637ab1086b4665294c630ac557
TUPAPPI <160808364+FRANCO-LQ@users.noreply.github.com>
2025-11-17 09:59:08 -0400

    Update Persona.java

--- SegundoParcial121/src/Persona.java
+++ SegundoParcial121/src/Persona.java
@@ -1,13 +1,34 @@
-
 public class Persona {
-	private String nombre;
-	private int edad;
-	private float PesoPersona;
-	public Persona(String nombre, int edad, float pesoPersona) {
-		super();
-		this.nombre = nombre;
-		this.edad = edad;
-		PesoPersona = pesoPersona;
-	}
+    private String nombre;
+    private int edad;
+    private double peso;
 
+    public Persona(String nombre, int edad, double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // ✔ TARIFA SEGÚN LO QUE TE PIDEN
+    public double calcularPasaje() {
+        if (edad >= 60) {     // Personas mayores de 60 años
+            return 1.50;
+        }
+        return 3.00;          // Todos los demás
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " | Edad: " + edad +
+               " | Peso: " + peso + "kg" +
+               " | Pasaje: " + calcularPasaje() + " Bs";
+    }
 }
