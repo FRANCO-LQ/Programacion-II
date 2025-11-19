006ee9eb10eae11abd90a3da4d081944d5130e06
TUPAPPI <160808364+FRANCO-LQ@users.noreply.github.com>
2025-11-19 08:45:29 -0400

    Update Linea.java

--- SegundoParcial121/src/Linea.java
+++ SegundoParcial121/src/Linea.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
 
 public class Linea {
-
+    private int nroLinea;
     private String color;
-    private ArrayList<Persona> personas;
     private ArrayList<Cabina> cabinas;
-    private int cantidadCabinas;
 
-    public Linea(String color, ArrayList<Persona> personas, ArrayList<Cabina> cabinas, int cantidadCabinas) {
+    public Linea(int nroLinea, String color) {
+        this.nroLinea = nroLinea;
         this.color = color;
-        this.personas = (personas != null) ? personas : new ArrayList<>();
-        this.cabinas = (cabinas != null) ? cabinas : new ArrayList<>();
-        this.cantidadCabinas = cantidadCabinas;
+        this.cabinas = new ArrayList<>();
+    }
+
+    // Add a Cabina to the Linea
+    public void agregarCabina(Cabina cabina) {
+        cabinas.add(cabina);
+    }
+
+    // Get all the Cabinas in this Linea
+    public ArrayList<Cabina> getCabinas() {
+        return cabinas;
+    }
+
+    @Override
+    public String toString() {
+        return "Linea " + nroLinea + " (" + color + ")";
     }
 }
